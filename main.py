cce7185e71d3c6e5dd341357565c754c28f5b35c
Saliou <gayesaliou29@gmail.com>
2022-06-28 20:39:28 +0200

    chore: add pdf generator

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 from dotenv import load_dotenv
 
+from pdf_generator import PdfGenerator
 from rds_writer import RdsWriter
 from timestream_reader import TimestreamReader
 
@@ -40,38 +41,45 @@ def get_rds_client(config):
 
 if __name__ == '__main__':
 
-    config = get_envs()
+    # config = get_envs()
 
-    tr = TimestreamReader(
-        access_key=config['aws_access_key'],
-        secret_key=config['aws_secret_key'],
-        database=config['timestream']['db'],
-        table=config['timestream']['table'],
-    )
+    # tr = TimestreamReader(
+    #     access_key=config['aws_access_key'],
+    #     secret_key=config['aws_secret_key'],
+    #     database=config['timestream']['db'],
+    #     table=config['timestream']['table'],
+    # )
 
-    data = tr.get_timestream_data()
+    # data = tr.get_timestream_data()
 
-    df = pd.DataFrame(data)
+    # df = pd.DataFrame(data)
 
-    data_ingested_today = len(df)
-    falls = 0
-    avg_serendipity = 0
-    location_density = []
+    # data_ingested_today = len(df)
+    # falls = 0
+    # avg_serendipity = 0
+    # location_density = []
 
-    if data_ingested_today == 0:
-        falls = int(df["nFall"].sum().item())
-        avg_serendipity = int(df['serendipity'].mean().item())
-        grouped_by_coords = df.groupby(['latitude', 'longitude']).size().reset_index(name='total').sort_values(by=['total'])
-        grouped_by_coords.apply(lambda x: location_density.append(json.loads(x.to_json())), axis=1)
+    # if data_ingested_today == 0:
+    #     falls = int(df["nFall"].sum().item())
+    #     avg_serendipity = int(df['serendipity'].mean().item())
+    #     grouped_by_coords = df.groupby(['latitude', 'longitude']).size().reset_index(name='total').sort_values(by=['total'])
+    #     grouped_by_coords.apply(lambda x: location_density.append(json.loads(x.to_json())), axis=1)
     
-    rds_client = get_rds_client(config)
+    # rds_client = get_rds_client(config)
 
-    rds_client.write_elaborated_data(
-        config['rds']['elaboration_table'],
-        data_ingested_today,
-        falls,
-        avg_serendipity,
-        location_density
-    )
+    # rds_client.write_elaborated_data(
+    #     config['rds']['elaboration_table'],
+    #     data_ingested_today,
+    #     falls,
+    #     avg_serendipity,
+    #     location_density
+    # )
+
+
+    pg = PdfGenerator()
+
+    pg.generate_report_pdf(dict())
+
+    
     
 
--- pdf_generator.py
+++ pdf_generator.py
@@ -1,4 +1,43 @@
+from re import template
+
+import jinja2
+from xhtml2pdf import pisa
+
+
 class PdfGenerator:
 
-    def generate_report_pdf():
+    def __init__(self) -> None:
+        
+        templateLoader = jinja2.FileSystemLoader(searchpath="./template")
+        templateEnv = jinja2.Environment(loader=templateLoader)
+        TEMPLATE_FILE = "index.html"
+        self.template = templateEnv.get_template(TEMPLATE_FILE)
+        self.output_file_name = "render.pdf"
+
+    # FIXME what to include in data?
+    def generate_report_pdf(self, data):
         """Generate PDF"""
+        html = self.__get_html(data)
+
+        err = self.__convert_html_to_pdf(html)
+
+        if err != 0:
+            raise Exception(f'Failed to generate report')
+
+
+    def __get_html(self, data):
+        source_html = self.template.render(json_data=data)
+        
+        return source_html
+
+    def __convert_html_to_pdf(self, sourceHtml):
+        """Convert to PDF"""
+        resultFile = open(self.output_file_name, "w+b")
+
+        pisa_status = pisa.CreatePDF(src=sourceHtml, dest=resultFile)
+
+        resultFile.close()
+
+        print(pisa_status.err)
+        
+        return pisa_status.err

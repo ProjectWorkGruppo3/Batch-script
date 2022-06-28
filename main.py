a610efb48eb667970ffaccd85b997c683c2a53db
Saliou <gayesaliou29@gmail.com>
2022-06-28 21:44:18 +0200

    chore: handle exception
    Close #3

--- main.py
+++ main.py
@@ -40,72 +40,76 @@ def get_envs():
 
 if __name__ == '__main__':
 
-    config = get_envs()
+    try:
 
-    tr = TimestreamReader(
-        access_key=config['aws_access_key'],
-        secret_key=config['aws_secret_key'],
-        database=config['timestream']['db'],
-        table=config['timestream']['table'],
-    )
+        config = get_envs()
 
-    rds_client = RdsWriter(
-        db=config['rds']['db'],
-        endpoint=config['rds']['endpoint'],
-        port=config['rds']['port'],
-        user=config['rds']['user'],
-        password=config['rds']['password'],
-    )
+        tr = TimestreamReader(
+            access_key=config['aws_access_key'],
+            secret_key=config['aws_secret_key'],
+            database=config['timestream']['db'],
+            table=config['timestream']['table'],
+        )
 
-    pg = PdfGenerator(config['outputFile'])
+        rds_client = RdsWriter(
+            db=config['rds']['db'],
+            endpoint=config['rds']['endpoint'],
+            port=config['rds']['port'],
+            user=config['rds']['user'],
+            password=config['rds']['password'],
+        )
 
-    s3_helper = S3Helper(
-        access_key=config['aws_access_key'],
-        secret_key=config['aws_secret_key'],
-        region=config['aws_region'],
-        bucket=config['s3']['bucket'],
-        folder=config['s3']['reports_folder'],
-    ) 
+        pg = PdfGenerator(config['outputFile'])
 
-    data = tr.get_timestream_data()
+        s3_helper = S3Helper(
+            access_key=config['aws_access_key'],
+            secret_key=config['aws_secret_key'],
+            region=config['aws_region'],
+            bucket=config['s3']['bucket'],
+            folder=config['s3']['reports_folder'],
+        ) 
 
-    df = pd.DataFrame(data)
+        data = tr.get_timestream_data()
 
-    data_ingested_today = len(df)
-    falls = 0
-    avg_serendipity = 0
-    location_density = []
+        df = pd.DataFrame(data)
 
-    if data_ingested_today == 0:
-        falls = int(df["nFall"].sum().item())
-        avg_serendipity = int(df['serendipity'].mean().item())
-        grouped_by_coords = df.groupby(['latitude', 'longitude']).size().reset_index(name='total').sort_values(by=['total'])
-        grouped_by_coords.apply(lambda x: location_density.append(json.loads(x.to_json())), axis=1)
-    
-    
-    # write to database
-    rds_client.write_elaborated_data(
-        config['rds']['elaboration_table'],
-        data_ingested_today,
-        falls,
-        avg_serendipity,
-        location_density
-    )
+        data_ingested_today = len(df)
+        falls = 0
+        avg_serendipity = 0
+        location_density = []
 
-
-    # generate report pdf
-    pg.generate_report_pdf({
-        'id': datetime.date.today(),
-        'generated_at': datetime.date.today(),
-        'data_ingested': data_ingested_today,
-        'falls': falls,
-        'serendipity': avg_serendipity
-    })
-
-    # upload report to s3
-    s3_helper.upload_pdf_report(config['outputFile'])
+        if data_ingested_today == 0:
+            falls = int(df["nFall"].sum().item())
+            avg_serendipity = int(df['serendipity'].mean().item())
+            grouped_by_coords = df.groupby(['latitude', 'longitude']).size().reset_index(name='total').sort_values(by=['total'])
+            grouped_by_coords.apply(lambda x: location_density.append(json.loads(x.to_json())), axis=1)
+        
+        
+        # write to database
+        rds_client.write_elaborated_data(
+            config['rds']['elaboration_table'],
+            data_ingested_today,
+            falls,
+            avg_serendipity,
+            location_density
+        )
 
 
-    pg.delete_report()
-    
+        # generate report pdf
+        pg.generate_report_pdf({
+            'id': datetime.date.today(),
+            'generated_at': datetime.date.today(),
+            'data_ingested': data_ingested_today,
+            'falls': falls,
+            'serendipity': avg_serendipity
+        })
 
+        # upload report to s3
+        s3_helper.upload_pdf_report(config['outputFile'])
+
+
+        pg.delete_report()
+
+    except Exception as e:
+        print('Script analysis failed. Error:')
+        print(e)
